6d30a977bbbf092cb4a09c03dd75e330f3f9a4ab
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2023-07-14 20:51:05 +0300

    Added simple url

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView
 
-# Create your views here.
+# class IndexView(DetailView):
+#     def get(self):
+#         return HttpResponse("Hello, world. You're at the polls index.")
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
