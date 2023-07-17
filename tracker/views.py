efe4156e7a9f1fbd99b399deb1fdda9bbbd035bb
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2023-07-17 18:28:29 +0300

    Added counter.

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.views.generic import DetailView
 
+from tracker.models import Ping
+
+
 class IndexView(DetailView):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello, world. You're at the tracker index.")
+        Ping.objects.create()
+        total = Ping.objects.count()
+        return HttpResponse(f"Hello, world. You're at the tracker index. {total}")
